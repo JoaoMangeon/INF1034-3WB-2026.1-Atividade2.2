1090d4c3cafe31ef39bff976fce851cdfb4d23ef
JoaoMangeon <jonyfera02@gmail.com>
2026-03-23 21:15:16 -0300

    Atividade 2.2

--- main.py
+++ main.py
@@ -2,16 +2,18 @@ from turtle import *
 from time import sleep
 
 t = Turtle()
-t.speed(50)
+t.speed(500)
 
+#Ucrania
 t.pu()
 t.goto(-250,125)
 t.pd()
-t.color("blue")
+t.color("#005BBB")
 t.begin_fill()
-t.fillcolor("blue")
+t.fillcolor("#005BBB")
+
 for _ in range(2):
- t.fd(400)
+ t.fd(402)
  t.rt(90)
  t.fd(126)
  t.rt(90)
@@ -20,12 +22,12 @@ t.end_fill()
 t.pu()
 t.goto(-250,-1)
 t.pd()
-t.color("yellow")
+t.color("#FFD500")
 t.begin_fill()
-t.fillcolor("yellow")
+t.fillcolor("#FFD500")
 
 for _ in range(2):
- t.fd(400)
+ t.fd(402)
  t.rt(90)
  t.fd(126)
  t.rt(90)
@@ -33,6 +35,7 @@ t.end_fill()
 sleep(2)
 t.clear()
 
+#Russia
 t.pu()
 t.goto(-250,125)
 t.pd()
@@ -41,7 +44,7 @@ t.begin_fill()
 t.fillcolor("white")
 
 for _ in range(2):
- t.fd(400)
+ t.fd(402)
  t.rt(90)
  t.fd(84)
  t.rt(90)
@@ -50,12 +53,12 @@ t.end_fill()
 t.pu()
 t.goto(-250,41)
 t.pd()
-t.color("blue")
+t.color("black")
 t.begin_fill()
-t.fillcolor("blue")
+t.fillcolor("#0033A0")
 
 for _ in range(2):
- t.fd(400)
+ t.fd(402)
  t.rt(90)
  t.fd(84)
  t.rt(90)
@@ -64,12 +67,12 @@ t.end_fill()
 t.pu()
 t.goto(-250,-43)
 t.pd()
-t.color("red")
+t.color("black")
 t.begin_fill()
-t.fillcolor("red")
+t.fillcolor("#DA291C")
 
 for _ in range(2):
- t.fd(400)
+ t.fd(402)
  t.rt(90)
  t.fd(84)
  t.rt(90)
@@ -77,15 +80,182 @@ t.end_fill()
 sleep(2)
 t.clear()
 
+#Itália
 t.pu()
 t.goto(-250,125)
 t.pd()
-t.color("#E05206")
+t.color("#008C45")
 t.begin_fill()
-t.fillcolor("#E05206")
+t.fillcolor("#008C45")
 
 for _ in range(2):
- t.fd(400)
+ t.fd(134)
+ t.rt(90)
+ t.fd(252)
+ t.rt(90)
+t.end_fill()
+
+t.pu()
+t.goto(-116,125)
+t.pd()
+t.color("#F4F5F0")
+t.begin_fill()
+t.fillcolor("#F4F5F0")
+
+for _ in range(2):
+ t.fd(134)
+ t.rt(90)
+ t.fd(252)
+ t.rt(90)
+t.end_fill()
+
+t.pu()
+t.goto(18,125)
+t.pd()
+t.color("#CD212A")
+t.begin_fill()
+t.fillcolor("#CD212A")
+
+for _ in range(2):
+ t.fd(134)
+ t.rt(90)
+ t.fd(252)
+ t.rt(90)
+t.end_fill()
+sleep(2)
+t.clear()
+
+#Polônia
+t.pu()
+t.goto(-250,125)
+t.pd()
+t.color("black")
+t.begin_fill()
+t.fillcolor("white")
+
+for _ in range(2):
+ t.fd(402)
+ t.rt(90)
+ t.fd(126)
+ t.rt(90)
+t.end_fill()
+
+t.pu()
+t.goto(-250,-1)
+t.pd()
+t.color("black")
+t.begin_fill()
+t.fillcolor("#DC143C")
+
+for _ in range(2):
+ t.fd(402)
+ t.rt(90)
+ t.fd(126)
+ t.rt(90)
+t.end_fill()
+sleep(2)
+t.clear()
+
+#Colômbia
+t.pu()
+t.goto(-250,125)
+t.pd()
+t.color("#FCD116")
+t.begin_fill()
+t.fillcolor("#FCD116")
+
+for _ in range(2):
+ t.fd(402)
+ t.rt(90)
+ t.fd(84)
+ t.rt(90)
+t.end_fill()
+
+t.pu()
+t.goto(-250,41)
+t.pd()
+t.color("#003893")
+t.begin_fill()
+t.fillcolor("#003893")
+
+for _ in range(2):
+ t.fd(402)
+ t.rt(90)
+ t.fd(84)
+ t.rt(90)
+t.end_fill()
+
+t.pu()
+t.goto(-250,-43)
+t.pd()
+t.color("#CE1126")
+t.begin_fill()
+t.fillcolor("#CE1126")
+
+for _ in range(2):
+ t.fd(402)
+ t.rt(90)
+ t.fd(84)
+ t.rt(90)
+t.end_fill()
+sleep(2)
+t.clear()
+
+#França
+t.pu()
+t.goto(-250,125)
+t.pd()
+t.color("black")
+t.begin_fill()
+t.fillcolor("#0055A4")
+
+for _ in range(2):
+ t.fd(134)
+ t.rt(90)
+ t.fd(252)
+ t.rt(90)
+t.end_fill()
+
+t.pu()
+t.goto(-116,125)
+t.pd()
+t.color("black")
+t.begin_fill()
+t.fillcolor("white")
+
+for _ in range(2):
+ t.fd(134)
+ t.rt(90)
+ t.fd(252)
+ t.rt(90)
+t.end_fill()
+
+t.pu()
+t.goto(18,125)
+t.pd()
+t.color("black")
+t.begin_fill()
+t.fillcolor("#EF4135")
+
+for _ in range(2):
+ t.fd(134)
+ t.rt(90)
+ t.fd(252)
+ t.rt(90)
+t.end_fill()
+sleep(2)
+t.clear()
+
+#Holanda
+t.pu()
+t.goto(-250,125)
+t.pd()
+t.color("black")
+t.begin_fill()
+t.fillcolor("#AE1C28")
+
+for _ in range(2):
+ t.fd(402)
  t.rt(90)
  t.fd(84)
  t.rt(90)
@@ -99,7 +269,7 @@ t.begin_fill()
 t.fillcolor("white")
 
 for _ in range(2):
- t.fd(400)
+ t.fd(402)
  t.rt(90)
  t.fd(84)
  t.rt(90)
@@ -108,12 +278,193 @@ t.end_fill()
 t.pu()
 t.goto(-250,-43)
 t.pd()
-t.color("#0DB02B")
+t.color("black")
+t.begin_fill()
+t.fillcolor("#21468B")
+
+for _ in range(2):
+ t.fd(402)
+ t.rt(90)
+ t.fd(84)
+ t.rt(90)
+t.end_fill()
+sleep(2)
+t.clear()
+
+#Gabão
+t.pu()
+t.goto(-250,125)
+t.pd()
+t.color("#009E60")
+t.begin_fill()
+t.fillcolor("#009E60")
+
+for _ in range(2):
+ t.fd(402)
+ t.rt(90)
+ t.fd(84)
+ t.rt(90)
+t.end_fill()
+
+t.pu()
+t.goto(-250,41)
+t.pd()
+t.color("#FCD116")
+t.begin_fill()
+t.fillcolor("#FCD116")
+
+for _ in range(2):
+ t.fd(402)
+ t.rt(90)
+ t.fd(84)
+ t.rt(90)
+t.end_fill()
+
+t.pu()
+t.goto(-250,-43)
+t.pd()
+t.color("#3A75C4")
+t.begin_fill()
+t.fillcolor("#3A75C4")
+
+for _ in range(2):
+ t.fd(402)
+ t.rt(90)
+ t.fd(84)
+ t.rt(90)
+t.end_fill()
+sleep(2)
+t.clear()
+
+#Irlanda
+t.pu()
+t.goto(-250,125)
+t.pd()
+t.color("black")
+t.begin_fill()
+t.fillcolor("#169B62")
+
+for _ in range(2):
+ t.fd(134)
+ t.rt(90)
+ t.fd(252)
+ t.rt(90)
+t.end_fill()
+
+t.pu()
+t.goto(-116,125)
+t.pd()
+t.color("black")
+t.begin_fill()
+t.fillcolor("white")
+
+for _ in range(2):
+ t.fd(134)
+ t.rt(90)
+ t.fd(252)
+ t.rt(90)
+t.end_fill()
+
+t.pu()
+t.goto(18,125)
+t.pd()
+t.color("black")
+t.begin_fill()
+t.fillcolor("#FF883E")
+
+for _ in range(2):
+ t.fd(134)
+ t.rt(90)
+ t.fd(252)
+ t.rt(90)
+t.end_fill()
+sleep(2)
+t.clear()
+
+#Nigéria
+t.pu()
+t.goto(-250,125)
+t.pd()
+t.color("black")
+t.begin_fill()
+t.fillcolor("#008751")
+
+for _ in range(2):
+ t.fd(134)
+ t.rt(90)
+ t.fd(252)
+ t.rt(90)
+t.end_fill()
+
+t.pu()
+t.goto(-116,125)
+t.pd()
+t.color("black")
+t.begin_fill()
+t.fillcolor("white")
+
+for _ in range(2):
+ t.fd(134)
+ t.rt(90)
+ t.fd(252)
+ t.rt(90)
+t.end_fill()
+
+t.pu()
+t.goto(18,125)
+t.pd()
+t.color("black")
+t.begin_fill()
+t.fillcolor("#008751")
+
+for _ in range(2):
+ t.fd(134)
+ t.rt(90)
+ t.fd(252)
+ t.rt(90)
+t.end_fill()
+sleep(2)
+t.clear()
+
+
+#Niger
+t.pu()
+t.goto(-250,125)
+t.pd()
+t.color("black")
+t.begin_fill()
+t.fillcolor("#E05206")
+
+for _ in range(2):
+ t.fd(402)
+ t.rt(90)
+ t.fd(84)
+ t.rt(90)
+t.end_fill()
+
+t.pu()
+t.goto(-250,41)
+t.pd()
+t.color("black")
+t.begin_fill()
+t.fillcolor("white")
+
+for _ in range(2):
+ t.fd(402)
+ t.rt(90)
+ t.fd(84)
+ t.rt(90)
+t.end_fill()
+
+t.pu()
+t.goto(-250,-43)
+t.pd()
+t.color("black")
 t.begin_fill()
 t.fillcolor("#0DB02B")
 
 for _ in range(2):
- t.fd(400)
+ t.fd(402)
  t.rt(90)
  t.fd(84)
  t.rt(90)
@@ -130,15 +481,17 @@ t.end_fill()
 sleep(2)
 t.clear()
 
+#Costa Rica
+
 t.pu()
 t.goto(-250,125)
 t.pd()
-t.color("#00205B")
+t.color("black")
 t.begin_fill()
 t.fillcolor("#00205B")
 
 for _ in range(2):
- t.fd(400)
+ t.fd(402)
  t.rt(90)
  t.fd(42)
  t.rt(90)
@@ -152,7 +505,7 @@ t.begin_fill()
 t.fillcolor("white")
 
 for _ in range(2):
- t.fd(400)
+ t.fd(402)
  t.rt(90)
  t.fd(42)
  t.rt(90)
@@ -161,12 +514,12 @@ t.end_fill()
 t.pu()
 t.goto(-250,41)
 t.pd()
-t.color("#EF3340")
+t.color("black")
 t.begin_fill()
 t.fillcolor("#EF3340")
 
 for _ in range(2):
- t.fd(400)
+ t.fd(402)
  t.rt(90)
  t.fd(84)
  t.rt(90)
@@ -180,7 +533,7 @@ t.begin_fill()
 t.fillcolor("white")
 
 for _ in range(2):
- t.fd(400)
+ t.fd(402)
  t.rt(90)
  t.fd(42)
  t.rt(90)
@@ -189,37 +542,229 @@ t.end_fill()
 t.pu()
 t.goto(-250,-85)
 t.pd()
-t.color("#00205B")
+t.color("black")
 t.begin_fill()
 t.fillcolor("#00205B")
 
 for _ in range(2):
- t.fd(400)
+ t.fd(402)
+ t.rt(90)
+ t.fd(42)
+ t.rt(90)
+t.end_fill()
+sleep(2)
+t.clear()
+
+#Botswana
+
+t.pu()
+t.goto(-250,125)
+t.pd()
+t.color("black")
+t.begin_fill()
+t.fillcolor("#75AADB")
+
+for _ in range(2):
+ t.fd(402)
+ t.rt(90)
+ t.fd(95)
+ t.rt(90)
+t.end_fill()
+
+t.pu()
+t.goto(-250,30)
+t.pd()
+t.color("black")
+t.begin_fill()
+t.fillcolor("white")
+
+for _ in range(2):
+ t.fd(402)
+ t.rt(90)
+ t.fd(10)
+ t.rt(90)
+t.end_fill()
+
+t.pu()
+t.goto(-250,20)
+t.pd()
+t.color("black")
+t.begin_fill()
+t.fillcolor("black")
+
+for _ in range(2):
+ t.fd(402)
  t.rt(90)
  t.fd(42)
  t.rt(90)
 t.end_fill()
 
+t.pu()
+t.goto(-250,-22)
+t.pd()
+t.color("black")
+t.begin_fill()
+t.fillcolor("white")
 
+for _ in range(2):
+ t.fd(402)
+ t.rt(90)
+ t.fd(10)
+ t.rt(90)
+t.end_fill()
 
+t.pu()
+t.goto(-250,-32)
+t.pd()
+t.color("black")
+t.begin_fill()
+t.fillcolor("#75AADB")
 
+for _ in range(2):
+ t.fd(402)
+ t.rt(90)
+ t.fd(95)
+ t.rt(90)
+t.end_fill()
+sleep(2)
+t.clear()
 
+#Azerbaijão
+t.pu()
+t.goto(-250,125)
+t.pd()
+t.color("#00B9E4")
+t.begin_fill()
+t.fillcolor("#00B9E4")
 
+for _ in range(2):
+ t.fd(402)
+ t.rt(90)
+ t.fd(84)
+ t.rt(90)
+t.end_fill()
 
+t.pu()
+t.goto(-250,41)
+t.pd()
+t.color("#ED2939")
+t.begin_fill()
+t.fillcolor("#ED2939")
 
+for _ in range(2):
+ t.fd(402)
+ t.rt(90)
+ t.fd(84)
+ t.rt(90)
+t.end_fill()
 
+t.pu()
+t.goto(-250,-43)
+t.pd()
+t.color("#3F9C35")
+t.begin_fill()
+t.fillcolor("#3F9C35")
 
+for _ in range(2):
+ t.fd(402)
+ t.rt(90)
+ t.fd(84)
+ t.rt(90)
+t.end_fill()
 
+t.pu()
+t.goto(-49,-30) 
+t.pd()
+t.color("white")
+t.begin_fill()
+t.fillcolor("white")
+t.circle(30)
+t.end_fill()
 
+t.pu()
+t.goto(-39,-24)
+t.pd()
+t.color("#ED2939")
+t.begin_fill()
+t.circle(24)
+t.end_fill()
 
+t.pu()
+t.goto(-18,4)
+t.rt(22.5)
+t.fd(10)
+t.lt(180)
+t.pd()
+t.color("white")
+t.begin_fill()
+t.fillcolor("white")
 
+for _ in range(8):
+    t.fd(20)
+    t.lt(135)
+t.end_fill()
+sleep(2)
+t.clear()
 
+#Mauricio
+t.pu()
+t.setheading(0)
+t.goto(-250,125)
+t.pd()
+t.color("#EA2839")
+t.begin_fill()
+t.fillcolor("#EA2839")
 
+for _ in range(2):
+ t.fd(402)
+ t.rt(90)
+ t.fd(63)
+ t.rt(90)
+t.end_fill()
 
+t.pu()
+t.goto(-250,62)
+t.pd()
+t.color("#1A206D")
+t.begin_fill()
+t.fillcolor("#1A206D")
 
+for _ in range(2):
+ t.fd(402)
+ t.rt(90)
+ t.fd(63)
+ t.rt(90)
+t.end_fill()
 
+t.pu()
+t.goto(-250,-1)
+t.pd()
+t.color("#FFD500")
+t.begin_fill()
+t.fillcolor("#FFD500")
 
+for _ in range(2):
+ t.fd(402)
+ t.rt(90)
+ t.fd(63)
+ t.rt(90)
+t.end_fill()
 
+t.pu()
+t.goto(-250,-64)
+t.pd()
+t.color("#00A551")
+t.begin_fill()
+t.fillcolor("#00A551")
+
+for _ in range(2):
+ t.fd(402)
+ t.rt(90)
+ t.fd(63)
+ t.rt(90)
+t.end_fill()
+
+#Angola
 
 
 
